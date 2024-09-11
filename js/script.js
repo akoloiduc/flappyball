90b5d54d8de564732f662ba20dc1d3a5480920e9
akoloiduc <hiroduc87@gmail.com>
2024-09-12 03:48:01 +0700

    flappy ball

--- js/script.js
+++ js/script.js
@@ -13,97 +13,81 @@ $(function(){
     var ball_height = parseInt(ball.height());
     var speed = 8;
     var go_up = false;
-    var score_updated = false;
+    var score_updated =false;
     var game_over = false;
-    
-    // Hàm bắt đầu trò chơi
     function playGame(){
         var the_game = setInterval(function(){
-            if(collision(ball, pole_1) || collision(ball, pole_2) || 
-               parseInt(ball.css('top')) <= 0 || 
-               parseInt(ball.css('top')) > container_height - ball_height) {
-                stop_the_game();
-            } else {
+            if(collision(ball, pole_1)|| collision(ball, pole_2)|| 
+            parseInt(ball.css('top')<=0)|| 
+            parseInt(ball.css('top'))> container_height - ball_height)
+            {stop_the_game();}
+            else{
                 var pole_current_position = parseInt(pole.css('right'));
-                if (pole_current_position > container_width - ball_left) {
-                    if (score_updated === false) {
-                        score.text(parseInt(score.text()) + 1);
-                        score_updated = true;
-                    }
-                }
-                if (pole_current_position > container_width) {
-                    var new_height = parseInt(Math.random() * 100);
-                    pole_1.css('height', pole_initial_height + new_height);
-                    pole_2.css('height', pole_initial_height - new_height);
-                    score_updated = false;
+                if(pole_current_position>container_width-ball_left)
+                {if(score_updated===false){
+                    score.text(parseInt(score.text())+1);
+                    score_updated = true;
+                }}
+                if(pole_current_position>container_width){
+                    var new_height = parseInt(Math.random()*100);
+                    pole_1.css('height',pole_initial_height+new_height);
+                    pole_2.css('height',pole_initial_height-new_height);
+                    score_updated=false;
                     pole_current_position = pole_initial_position;
                 }
                 pole.css('right', pole_current_position + speed);
-                if (go_up === false) {
+                if(go_up === false){
                     go_down();
                 }
             }
-        }, 40);
+        },40);
     }
-
-    // Xử lý sự kiện touch và mouse
-    function startFlying() {
-        go_up = setInterval(up, 40);
-    }
-
-    function stopFlying() {
+    $('#container').mouseup(function(e){
         clearInterval(go_up);
         go_up = false;
-    }
-
-    // Xử lý sự kiện trên di động và máy tính
-    $('#container').on('mousedown touchstart', function() {
-        startFlying();
     });
-
-    $('#container').on('mouseup touchend', function() {
-        stopFlying();
+    $('#container').mousedown(function(e){
+        go_up = setInterval(up,40);
     });
-
     $('#play_btn').on('click touchstart', function(){
         playGame();
         $(this).hide();
     });
-
     function go_down(){
-        ball.css('top', parseInt(ball.css('top')) + 10);
+        ball.css('top', parseInt(ball.css('top'))+10);
         ball.css('transform', 'rotate(50deg)');
     }
-
     function up(){
-        ball.css('top', parseInt(ball.css('top')) - 20);
+        ball.css('top', parseInt(ball.css('top'))-20);
         ball.css('transform', 'rotate(-10deg)');
     }
-
     function stop_the_game(){
         clearInterval(playGame());
         game_over = true;
         $('#restart_btn').slideDown();
     }
-
-    $('#restart_btn').on('click touchstart', function(){
+    $("#restart_btn").on('click touchstart', function(){
         location.reload();
     });
-    // Hàm kiểm tra va chạm
-    function collision($div1, $div2) {
+    function collision($div1, $div2){
         var x1 = $div1.offset().left;
         var y1 = $div1.offset().top;
         var h1 = $div1.outerHeight(true);
         var w1 = $div1.outerWidth(true);
-        var b1 = y1 + h1;
-        var r1 = x1 + w1;
+        var b1 = y1+h1;
+        var r1 = x1+w1;
         var x2 = $div2.offset().left;
         var y2 = $div2.offset().top;
         var h2 = $div2.outerHeight(true);
         var w2 = $div2.outerWidth(true);
-        var b2 = y2 + h2;
-        var r2 = x2 + w2;
-
-        return !(b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2);
+        var b2 = y2+h2;
+        var r2 = x2 +w2;
+        if(b1<y2||y1>b2||r1<x2||x1>r2){
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 });
